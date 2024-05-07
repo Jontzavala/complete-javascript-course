b922793e0834fded3757ef27cf527765ade9cded
Jonathan Zavala <70041382+Jontzavala@users.noreply.github.com>
2024-05-07 15:12:31 -0500

    First assignment done

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,10 @@
+// Values and Variables﻿
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// Log their values to the console.
+
+let country = "USA";
+let continent = "North America";
+let population = 100;
+
+console.log(country + " is on the continent " + continent + " with a population of " + population);
