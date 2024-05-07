4d2edffd5a51d80f2dc41857fa340c322ec332eb
Jonathan Zavala <70041382+Jontzavala@users.noreply.github.com>
2024-05-07 15:24:03 -0500

    Assignment 2 done

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -8,3 +8,14 @@ let continent = "North America";
 let population = 100;
 
 console.log(country + " is on the continent " + continent + " with a population of " + population);
+
+
+// Data Types﻿
+// Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+// Log the types of isIsland, population, country and language to the console.
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland, typeof population, typeof country, typeof language);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -14,3 +15,27 @@ let myFirstJob = "Programmer";
 let myCurrentJob = "Teacher";
 
 console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof "Fun");
+// console.log(typeof 23);
+
+javascriptIsFun = "YES!";
+
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
