c7fc018460e74156a95e73783b14f2c4c641028f
Jontzavala <jontzavala@gmail.com>
2024-05-23 07:04:28 -0500

    Functions and arrays done

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,115 @@
-'use strict';
+"use strict";
+/*
+const bookings = [];
+
+// Default Parameters
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //   ES5
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 3);
+
+createBooking('LH123', undefined, 1000);
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas S',
+  passport: 23242324,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 23242324) {
+    alert('Checked In');
+  } else {
+    alert('Wrong Passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// // Is the same as doing ...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+const oneWord = function (str) {
+  return str.replaceAll(' ', '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string; ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// This is the same this as doing what is below just shorter
+// const greet = greeting => {
+//   return name => {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+// This is without an arrow function
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+const greeterHey = greet("Hey");
+greeterHey("Jonas");
+greeterHey("Steven");
+
+greet("Hello")("Jonas");
+
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+*/
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,6 +71,180 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+/*
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// Slice
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+// Splice changes original array
+// console.log(arr.splice(2));
+console.log(arr.splice(-1));
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// Reverse also mutates the orginal array
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// Concat
+const letters = arr.concat(arr2);
+console.log(letters);
+
+// Join
+console.log(letters.join(' - '));
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// at()
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1));
+console.log(arr.at(-1));
+console.log(arr.at(-2));
+
+// also works on strings
+
+console.log('Jonas'.at(0));
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---------------------');
+
+movements.forEach(function (movement, index, array) {
+  if (movement > 0) {
+    console.log(`Movement ${index}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index}: You withdrew ${Math.abs(movement)}`);
+  }
+});
+
+// Map method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const euroToUsd = 1.1;
+
+const movementsUsd = movements.map(function (movement) {
+  return movement * euroToUsd;
+});
+
+// Arrow function version
+// const movementsUsd = movements.map(movement => movement * euroToUsd);
+
+console.log(movements);
+console.log(movementsUsd);
+
+const movementsUsdFor = [];
+for (const mov of movements) movementsUsdFor.push(mov * euroToUsd);
+console.log(movementsUsdFor);
+
+const movementDescp = movements.map(
+  (mov, index) =>
+    `Movement ${index + 1}: You ${
+      mov > 0 ? 'deposited' : 'withdrew'
+    } ${Math.abs(mov)}`
+);
+console.log(movementDescp);
+
+//filter method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(mov => mov > 0);
+
+const withdraws = movements.filter(mov => mov < 0);
+
+console.log(movements);
+console.log(deposits);
+console.log(withdraws);
+
+
+// Reduce method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(movements);
+
+// accumulator is like a snowball, the zero at the end is telling where the accumulator to start
+// const globalBalance = movements.reduce(function (acc, current, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + current;
+// }, 0);
+
+// arrow function version
+const balance = movements.reduce((acc, curr) => acc + curr, 0);
+
+// console.log(globalBalance);
+console.log(balance);
+
+// Chaining methods
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const euroToUsd = 1.1;
+
+// Pipeline
+const total = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * euroToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+
+console.log(total);
+
+*/
+// sorting strings (puts it in alphabetical order)
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+
+console.log(owners.sort());
+console.log(owners);
+
+// sorting numbers
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(movements);
+
+// return < 0, A, B (keep order)
+// return > 0, B, A (swithc order)
+
+// Ascending
+movements.sort((a, b) => a - b);
+console.log(movements);
+// console.log(
+//   movements.sort((a, b) => {
+//     if (a > b) {
+//       return 1;
+//     }
+//     if (b > a) {
+//       return -1;
+//     }
+//   })
+// );
+
+// Decending
+movements.sort((a, b) => b - a);
+console.log(movements);
+// console.log(
+//   movements.sort((a, b) => {
+//     if (a > b) {
+//       return -1;
+//     }
+//     if (b > a) {
+//       return 1;
+//     }
+//   })
+// );
